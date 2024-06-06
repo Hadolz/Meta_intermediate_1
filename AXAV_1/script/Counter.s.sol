4b5519e9f12c8f95d181465ac0115584369e43ae
Hadolz <moyosore815@gmail.com>
2024-06-06 01:12:55 +0100

    Update Counter.s.sol

--- AXAV_1/script/Counter.s.sol
+++ AXAV_1/script/Counter.s.sol
@@ -1,12 +1,73 @@
-// SPDX-License-Identifier: UNLICENSED
-pragma solidity ^0.8.13;
+// SPDX-License-Identifier: MIT
+pragma solidity ^0.8.17;
 
-import {Script, console} from "forge-std/Script.sol";
+contract TaskManager {
 
-contract CounterScript is Script {
-    function setUp() public {}
+    error FUNCTION_ONLY_FOR_OWNER();
 
-    function run() public {
-        vm.broadcast();
+    struct TaskDetails{
+        string name;
+        string details;
+        bool status;
     }
+
+    TaskDetails[] public tasks;
+
+    address private manager;
+
+    constructor() {
+        manager = msg.sender;
+    }
+
+    function restrictToOwner() private view {
+        if (msg.sender!= manager){
+            revert FUNCTION_ONLY_FOR_OWNER();
+        }
+    }
+
+    function addTask(string calldata _name, string calldata _details) external  {
+        restrictToOwner();
+        tasks.push(TaskDetails(_name,_details,false));
+    }
+
+    function editTask(uint _id, string calldata _name, string calldata _details) external  {
+        restrictToOwner();
+        require(_id < tasks.length, "Invalid task index.");
+        tasks[_id] = TaskDetails(_name, _details, false);
+    }
+
+    function toggleStatus(uint _id) external  {
+        restrictToOwner();
+        require(_id < tasks.length, "Invalid task index.");
+        tasks[_id].status =!tasks[_id].status;
+    }
+
+    function fetchTask(uint _id) external view returns(string memory, string memory, bool){
+        require(_id < tasks.length, "Invalid task index.");
+        TaskDetails memory task = tasks[_id];
+        return (task.name, task.details, task.status);
+    }
+
+    function removeTask(uint _id) external  {
+        restrictToOwner();
+        require(_id < tasks.length, "Invalid task index.");
+        delete tasks[_id];
+    }
+
+    function retrieveAllTasks() external view returns(TaskDetails[] memory){
+        return tasks;
+    }
+
+    // Ensures internal integrity using assert
+    function verify() external view {
+        restrictToOwner();
+        uint256 actualCount = tasks.length;
+        for (uint i = 0; i < tasks.length; i++) {
+            assert(tasks[i].status == false || tasks[i].status == true);
+        }
+        if (actualCount!= tasks.length) {
+            revert("Detected discrepancy in array size.");
+        }
+    }
+
 }
